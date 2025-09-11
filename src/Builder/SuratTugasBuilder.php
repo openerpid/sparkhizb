3e4f2cd57295d52f40383da919b4037ead8be52b
M. Ali Imron <lienux.qq@gmail.com>
2025-09-11 16:26:12 +0700

    update

---
+++ src/Builder/SuratTugasBuilder.php
@@ -0,0 +1,243 @@
+<?php
+
+namespace Sparkhizb\Builder;
+
+use Sparkhizb\Helpers\IdentityHelper;
+use Sparkhizb\Helpers\UmmuHelper;
+use Sparkhizb\Helpers\RequestHelper;
+use Sparkhizb\UmmuGoodsEvaluation;
+use Dorbitt\Openapi2\UmmuSuratTugas;
+
+class SuratTugasBuilder
+{
+    public function __construct()
+    {
+        $this->db = \Config\Database::connect();
+        $this->iescm = \Config\Database::connect('iescm');
+        $this->request = \Config\Services::request();
+        $this->reqH = new RequestHelper();
+        $this->identity = new IdentityHelper();
+        $this->umHelp = new UmmuHelper();
+        $this->ummu = new UmmuSuratTugas();
+    }
+
+    public function show($id = null)
+    {
+        // $payload = [
+        //     "limit" => 10,
+        //     "offset" => 0,
+        //     "sort" => "id",
+        //     "order" => "desc",
+        //     "search" => "",
+        //     "selects" => "*"
+        // ];
+
+        // $params = [
+        //     "id" => $id,
+        //     "payload" => $payload,
+        //     "token" => $this->reqH->myToken()
+        // ];
+
+        // $builder = $this->ummu->show($params);
+
+        // return $builder;
+    }
+
+    public function show_from_openapi2($id = null, $payload)
+    {
+        $params = [
+            "id" => $id,
+            "payload" => $payload,
+            "token" => $this->reqH->myToken()
+        ];
+
+        $builder = $this->ummu->show($params);
+
+        return $builder;
+    }
+
+    public function show_herpapi($id = null)
+    {
+        $payload = [
+            "limit" => 10,
+            "offset" => 0,
+            "sort" => "id",
+            "order" => "desc",
+            "search" => "",
+            "selects" => "*"
+        ];
+
+        $params = [
+            "id" => $id,
+            "payload" => $payload,
+            "token" => $this->identity->login_token()
+        ];
+
+        $builder = $this->ummu->show($params);
+
+        return $builder;
+    }
+
+    public function show_new($nik, $site)
+    {
+        $builder = $this->mNum
+            ->where('nik', $nik)
+            ->where('site', $site)
+            ->where('number IS NULL')
+            ->where('used_at IS NULL')
+            ->where('YEAR(created_at)', date('Y'))
+            ->where('MONTH(created_at)', date('m'))
+            ->first();
+
+        return $builder;
+    }
+
+    public function show_number_unused($nik, $site)
+    {
+        $builder = $this->mNum
+            ->where('nik', $nik)
+            ->where('site', $site)
+            ->where('used_at IS NULL')
+            ->where('YEAR(created_at)', date('Y'))
+            ->where('MONTH(created_at)', date('m'))
+            ->first();
+
+        return $builder;
+    }
+
+    public function getLastRow()
+    {
+        $builder = $this->iescm->table($this->mNum->table)
+            ->where('YEAR(created_at)', date('Y'))
+            ->where('MONTH(created_at)', date('m'))
+            ->get()
+            ->getLastRow();
+
+        return $builder;
+    }
+
+    public function update_new($id, $payload)
+    {
+        $builder = $this->mNum
+            ->where('id', $id)
+            ->set($payload)
+            ->update();
+
+        return $builder;
+    }
+
+    public function create_id($payload)
+    {
+        return $this->mNum->insert($payload);
+    }
+
+    public function insert_number($payload)
+    {
+        return $this->mNum->insert($payload);
+    }
+
+    public function used_number($number)
+    {
+        return $this->mNum
+            ->where('number', $number)
+            ->set('used_at', date('Y-m-d H:i:s'))
+            ->update();
+    }
+
+    public function insert($payload)
+    {
+        $params = [
+            "payload" => $payload,
+            "token" => $this->umHelp->token()
+        ];
+
+        $builder = $this->ummu->insert($params);
+
+        return $builder;
+    }
+
+    public function zoneCreate_insert($payload)
+    {
+        $params = [
+            "payload" => $payload,
+            "token" => $this->umHelp->token()
+        ];
+
+        $builder = $this->ummu->zoneCreate_insert($params);
+
+        return $builder;
+    }
+
+    public function zoneCreate_show($id = null, $payload)
+    {
+        $params = [
+            "id" => $id,
+            "payload" => $payload,
+            "token" => $this->reqH->myToken()
+        ];
+
+        $builder = $this->ummu->zoneCreate_show($params);
+
+        return $builder;
+    }
+
+    public function zoneCreate_update($id, $payload)
+    {
+        $params = [
+            "id" => $id,
+            "payload" => $payload,
+            "token" => $this->umHelp->token()
+        ];
+
+        $builder = $this->ummu->zoneCreate_update($params);
+
+        return $builder;
+    }
+
+
+
+    /**
+     * Process Zone*/
+    public function zoneProcess_show($id = null, $payload)
+    {
+        $params = [
+            "id" => $id,
+            "payload" => $payload,
+            "token" => $this->reqH->myToken()
+        ];
+
+        $builder = $this->ummu->zoneProcess_show($params);
+
+        return $builder;
+    }
+
+    public function zoneProcess_update($id, $payload)
+    {
+        $params = [
+            "id" => $id,
+            "payload" => $payload,
+            "token" => $this->umHelp->token()
+        ];
+
+        $builder = $this->ummu->zoneProcess_update($params);
+
+        return $builder;
+    }
+
+
+
+    /**
+     * Monitoring Zone*/
+    public function zoneMonitoring_show($id = null, $payload)
+    {
+        $params = [
+            "id" => $id,
+            "payload" => $payload,
+            "token" => $this->reqH->myToken()
+        ];
+
+        $builder = $this->ummu->zoneMonitoring_show($params);
+
+        return $builder;
+    }
+}
---
+++ src/Models/PlafonPenginapanModel.php
@@ -0,0 +1,83 @@
+<?php
+
+namespace Sparkhizb\Models;
+
+use CodeIgniter\Model;
+
+class PlafonPenginapanModel extends Model
+{
+    protected $DBGroup = 'iescm';
+    protected $table = 'plafon_penginapan';
+    protected $primaryKey = 'id';
+    protected $useAutoIncrement = true;
+    protected $insertID = 0;
+    protected $returnType = 'array';
+    protected $useSoftDeletes = true;
+    protected $protectFields = true;
+    protected $allowedFields = [
+        "id",
+        "company_id",
+        "nomor_dokumen",
+        "insident_classification",
+        "site",
+        "devisi",
+        "departemen",
+        "section",
+        "tanggal_kejadian",
+        "tanggal_pelaporan",
+        "waktu_pelaporan",
+        "shift_kerja",
+        "mulai_shift",
+        "selesai_shift",
+        "waktu_kejadian",
+        "lokasi_insiden",
+        "detail_lokasi_insiden",
+        "cidera",
+        "detail_cidera_lainnya",
+        "kronologi",
+        "instansi_pemerintah",
+        "nama_pejabat",
+        "pemerintah_disampaikan_oleh",
+        "pemerintah_tanggal",
+        "asuransi",
+        "nama_perusahaan",
+        "asuransi_disampaikan_oleh",
+        "asuransi_tanggal",
+        "pihak_tiga",
+        "nama_pihak_ketiga",
+        "pihak_tiga_disampaikan_oleh",
+        "pihak_tiga_tanggal",
+        "tipe_insiden",
+        "penjelasan_insiden",
+        "created_at",
+        "updated_at",
+        "deleted_at",
+        "created_by",
+        "updated_by",
+        "deleted_by"
+    ];
+
+    // Dates
+    protected $useTimestamps = true;
+    protected $dateFormat = 'datetime';
+    protected $createdField = 'created_at';
+    protected $updatedField = 'updated_at';
+    protected $deletedField = 'deleted_at';
+
+    // Validation
+    protected $validationRules = [];
+    protected $validationMessages = [];
+    protected $skipValidation = false;
+    protected $cleanValidationRules = true;
+
+    // Callbacks
+    protected $allowCallbacks = true;
+    protected $beforeInsert = [];
+    protected $afterInsert = [];
+    protected $beforeUpdate = [];
+    protected $afterUpdate = [];
+    protected $beforeFind = [];
+    protected $afterFind = [];
+    protected $beforeDelete = [];
+    protected $afterDelete = [];
+}
